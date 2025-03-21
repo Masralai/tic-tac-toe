d6a254071b9f4625304a9b6b2dd7a3a11bc7a332
Masralai <devdeeppaul2004@gmail.com>
2025-03-21 21:54:53 +0530

    works

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,63 @@ import "./index.css";
 import Block from "./components/Block";
 
 const App: React.FC = () => {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [currentTurn, setCurrentTurn] = useState("X");
 
-  const [state,setState] = useState(Array(9).fill(null))
-  const [currentTurn,setCurrentTurn] = useState("X")
-  
-  
-  const handleBlockClick=(index:number)=>{
-    const stateCopy = Array.from(state)
-    stateCopy[index] = currentTurn
-    setCurrentTurn(currentTurn==='X'?'O':'X')
-    setState(stateCopy)
-  }
+  const checkWinner = (state: any[]) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return true;
+      }
+    }
+    return false;
+  };
 
-  console.log(state)
+  const handleBlockClick = (index: number) => {
+    const stateCopy = Array.from(state);
+    if (stateCopy[index] !== null) return;
+    stateCopy[index] = currentTurn;
+
+    //winner check
+    const win = checkWinner(stateCopy);
+    if (win) {
+      alert(`${currentTurn} won the game`);
+    }
+
+    setCurrentTurn(currentTurn === "X" ? "O" : "X");
+    setState(stateCopy);
+  };
+
+  console.log(state);
 
   return (
     <div className="p-5">
       <div className="flex">
-        <Block  onClick={()=>handleBlockClick(0)} value={state[0]}/>
-        <Block  onClick={()=>handleBlockClick(1)} value={state[1]}/>
-        <Block  onClick={()=>handleBlockClick(2)} value={state[2]}/>
+        <Block onClick={() => handleBlockClick(0)} value={state[0]} />
+        <Block onClick={() => handleBlockClick(1)} value={state[1]} />
+        <Block onClick={() => handleBlockClick(2)} value={state[2]} />
       </div>
       <div className="flex">
-        <Block  onClick={()=>handleBlockClick(3)} value={state[3]}/>
-        <Block  onClick={()=>handleBlockClick(4)} value={state[4]}/>
-        <Block  onClick={()=>handleBlockClick(5)} value={state[5]}/>
+        <Block onClick={() => handleBlockClick(3)} value={state[3]} />
+        <Block onClick={() => handleBlockClick(4)} value={state[4]} />
+        <Block onClick={() => handleBlockClick(5)} value={state[5]} />
       </div>
       <div className="flex">
-        <Block  onClick={()=>handleBlockClick(6)} value={state[6]}/>
-        <Block  onClick={()=>handleBlockClick(7)} value={state[7]}/>
-        <Block  onClick={()=>handleBlockClick(8)} value={state[8]}/>
+        <Block onClick={() => handleBlockClick(6)} value={state[6]} />
+        <Block onClick={() => handleBlockClick(7)} value={state[7]} />
+        <Block onClick={() => handleBlockClick(8)} value={state[8]} />
       </div>
-      
     </div>
   );
 };
--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
-
 interface BlockProps {
-    value: string | null,
-    onClick?:() =>void
+  value: string | null;
+  onClick?: () => void;
 }
 
-
 const Block: React.FC<BlockProps> = (props) => {
-  
   return (
-    <div  onClick={props.onClick}  className="flex justify-center items-center font-medium text-4xl border-2 border-solid h-25 w-25 cursor-pointer">
+    <div
+      onClick={props.onClick}
+      className="flex justify-center items-center font-medium text-4xl border-2 border-solid h-25 w-25 cursor-pointer"
+    >
       {props.value}
     </div>
   );
